968c7d874d93112e42a58c5e01dcaa70cf79f0b5
Ivan <telitsinivan05@yandex.ru>
2023-01-08 22:01:42 +0300

    commit

---
+++ check_u_a.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup
+
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
+driver.get('https://ciox.ru/check-user-agent')
+soup = BeautifulSoup(driver.page_source, 'lxml')
+u_a = str(soup.find('br').next)
+print(u_a.lstrip().rstrip())
